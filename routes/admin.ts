96ea30c2f34d340d2d2a665d458c5c3f19120cab
Nothile Moyo <Nothile1@gmail.com>
2023-03-13 20:41:45 +0000

    Parsed and served HTML document

--- routes/admin.ts
+++ routes/admin.ts
@@ -1,6 +1,7 @@
 // Import express router for the admin and shop pages
 // This file is for the product routes
 import express from "express";
+import fs from "fs";
 
 // import our express types for TypeScript use
 import { Request, Response, NextFunction } from 'express';
@@ -11,13 +12,16 @@ const adminRoutes = express.Router();
 // Initial middleware response
 adminRoutes.get("/add-product", (request : Request, response : Response, next : NextFunction) => {
 
+    // Get the add product HTML page by parsing the file
+    // We turn the file into a buffer instance and pass it through to the add-product page
+    const data = fs.readFileSync("./views/add-product.html");
+    console.log("data is below");
+    console.log(data.toString());
+
     // Define an action and a method for the form
-    response.send(`
-        <form action="/admin/add-product" method="POST">
-            <input type="text" name="title"/>
-            <button type="submit">Add Product</button>
-        </form>
-    `);
+    response.writeHead(200, {"Content-Type" : "text/html"});
+    response.write(data.toString());
+    response.end();
 });
 
 // Handle a response in the body of a request usng middleware
@@ -26,7 +30,6 @@ adminRoutes.post("/add-product", (request : Request, response : Response, next :
     console.log(request.body.title);
     response.redirect("/");
 });
-
 
 
 export default adminRoutes;
