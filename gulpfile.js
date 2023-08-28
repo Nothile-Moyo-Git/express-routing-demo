01b70c5579c32527fe1f2d74d891b2c21a50ca9c
Nothile Moyo <Nothile1@gmail.com>
2023-08-28 23:51:33 +0100

    Started migration over to minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 'use strict';
 
+// Import gulp model
 const gulp = require('gulp');
 
-const sass = require('gulp-sass')(require('node-sass'));
+// Import sass model
+const sass = require('gulp-sass')(require('sass'));
 
-const concat = require('gulp-concat');
+// Import minification model
+const csso = require('gulp-csso');
 
-sass.compiler = require('node-sass');
+// Define our compiler model
+sass.compiler = require('sass');
 
+// Compile the scss into css
 gulp.task('sass', function () {
 
-   return gulp.src('src/sass/**/*.scss')
-   .pipe(concat('custom.scss'))
+   // Find the source of the file
+   // Concat it by adding the appropriate new lines
+   // Compile the scss code into css with compression in order to reduce the size of the css file
+   // Output the newly created css file to the directory
+
+   return gulp.src('src/sass/**/custom.scss')
    .pipe(sass().on('error', sass.logError))
    .pipe(gulp.dest('src/css'));
 });
@@ -19,5 +28,6 @@ gulp.task('sass', function () {
 // Gulp watcher, automatically runs
 gulp.task('sass:watch', function () {
 
+   // Run the above method continuouly anytime there's a chance to the scss files
    gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
 });
