f427d6f83e71ec28868aeadc86d1bb06014e2c81
Nothile Moyo <Nothile1@gmail.com>
2023-10-16 20:52:48 +0100

    Finished getting the URL verification to work, used regexp and a utility method instead of isurl

--- src/util/utility-methods.ts
+++ src/util/utility-methods.ts
@@ -1,6 +1,10 @@
 /**
  * This is the utility methods file
  * This creates re-usable functions for different functionality which can be queried from here instead of in each file
+ * 
+ * @method isInt = (number : any) => boolean
+ * @method isFloat = (number : any) => boolean
+ * @method isValidUrl = (number : any) => boolean
  */
 
 export const isInt = (number : any) => {
@@ -9,4 +13,14 @@ export const isInt = (number : any) => {
 
 export const isFloat = (number : any) => {
     return Number(number) === number && number % 1 !== 0;
+};
+
+export const isValidUrl = (url : string) => {
+    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+    return !!urlPattern.test(url);
 };
