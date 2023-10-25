1333fcf9140ad61a14bdbf724297c0378657fdee
Nothile Moyo <Nothile1@gmail.com>
2023-10-25 21:11:26 +0100

    Implemented basic bubbles in the background

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -1,3 +1,19 @@
+/**
+ * Author : Nothile Moyo
+ * Date Written : 25/10/2023
+ * License : MIT
+ *
+ * Admin Controller, handles the requests to do with administration ( adding products etc... )
+ *
+ * @method getAddProduct = ( request : ExtendedRequestInterface, response : Response ) => void
+ * @method postAddProduct = ( request : ExtendedRequestInterface, response : Response ) => void | response : Response
+ * @method getProducts = ( request : ExtendedRequestInterface, response : Response ) => void
+ * @method updateProductController = ( request : ExtendedRequestInterface, response : Response ) => void | response : Response
+ * @method deleteProduct = ( request : ExtendedRequestInterface, response : Response ) => void | response : Response
+ * @method getAdminEditProduct = ( request : ExtendedRequestInterface, response : Response ) => void
+ */
+
+
 // import our express types for TypeScript use
 import { Response } from 'express';
 import Product from '../models/products';
@@ -272,4 +288,47 @@ const deleteProduct = async ( request : ExtendedRequestInterface, response : Res
     }
 };
 
-export { getAddProduct, postAddProduct, getProducts, updateProductController, deleteProduct }; 
+// Get admin edit product controller
+const getAdminEditProduct = async ( request : ExtendedRequestInterface, response : Response ) => {
+
+    // Get our request session from our Mongoose database and check if we're logged in
+    const isLoggedIn = request.session.isLoggedIn;
+
+    // csrfToken from our session
+    const csrfToken = request.session.csrfToken;
+
+    // Check if our Object id is valid in case we do onto a bad link
+    // This is more of a pre-emptive fix for production builds
+    const isObjectIdValid = ObjectId.isValid(request.params.id);
+
+    // Create a new product Id and guard it
+    const productId = isObjectIdValid ? new ObjectId(request.params.id) : null;
+
+    // Get single product details
+    const singleProduct = isObjectIdValid ? await Product.findById(productId) : null;
+
+    // Check if we have products
+    const hasProduct = singleProduct !== null;
+
+    // Render the edit products template
+    response.render(      
+        "pages/admin/edit-product", 
+        { 
+            pageTitle : "Edit Products", 
+            id : productId, 
+            productInformation : singleProduct,
+            hasProducts : hasProduct,
+            isAuthenticated : isLoggedIn === undefined ? false : true,
+            csrfToken : csrfToken,
+            isSubmitted : false,
+            inputsValid : {
+                isTitleValid : true,
+                isImageValid : true,
+                isDescriptionValid : true,
+                isPriceValid : true
+            }
+        }
+    );
+};
+
+export { getAddProduct, postAddProduct, getProducts, updateProductController, deleteProduct, getAdminEditProduct }; 
